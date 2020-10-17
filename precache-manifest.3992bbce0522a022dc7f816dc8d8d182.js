fdaacfca2d7b07009a09ab3d99b6cd3882b1a5bf
sian-0018 <sian-0018@gms.tku.edu.tw>
2020-10-18 00:12:24 +0800

    Updates

---
+++ precache-manifest.3992bbce0522a022dc7f816dc8d8d182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1ffbd01df5d20d97584dff5692fadc",
+    "url": "/ClassDome_33/index.html"
+  },
+  {
+    "revision": "afd4e78530aa7d6e0d0d",
+    "url": "/ClassDome_33/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7a46f7ce1e64d1c2200a",
+    "url": "/ClassDome_33/static/css/main.ba680a24.chunk.css"
+  },
+  {
+    "revision": "afd4e78530aa7d6e0d0d",
+    "url": "/ClassDome_33/static/js/2.329d268f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClassDome_33/static/js/2.329d268f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a46f7ce1e64d1c2200a",
+    "url": "/ClassDome_33/static/js/main.3b0cc612.chunk.js"
+  },
+  {
+    "revision": "c5fd98c75e57f09276e6",
+    "url": "/ClassDome_33/static/js/runtime-main.d6f27e64.js"
+  },
+  {
+    "revision": "e56930b33f8ead0ff1da12c00be25fcf",
+    "url": "/ClassDome_33/static/media/origami_making.e56930b3.ttf"
+  }
+]);
